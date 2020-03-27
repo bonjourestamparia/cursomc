602927f203a0ab0b78e25bae2e10fb8e93596aca
Rodrigo Bonjour <rodrigo@bonjourestamparia.com.br>
2020-03-27 18:58:15 -0300

    Criando operação de instanciação

--- src/main/java/com/bonjourestamparia/CursomcApplication.java
+++ src/main/java/com/bonjourestamparia/CursomcApplication.java
@@ -1,13 +1,28 @@
 package com.bonjourestamparia;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bonjourestamparia.domain.Categoria;
+import com.bonjourestamparia.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		categoriaRepository.saveAll(Arrays.asList(new Categoria("Informática"), new Categoria("Escritório")));
 	}
 
 }
