83190a1176a642b2d22343648c4c97ba440ebdd9
Rodrigo Bonjour <rodrigo@bonjourestamparia.com.br>
2020-03-29 13:43:55 -0300

    Ajuste no endpoint categoria/{id}

--- src/main/java/com/bonjourestamparia/services/CategoriaService.java
+++ src/main/java/com/bonjourestamparia/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.bonjourestamparia.domain.Categoria;
 import com.bonjourestamparia.repositories.CategoriaRepository;
+import com.bonjourestamparia.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,6 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> cat = repo.findById(id);
-		return cat.orElse(null);
+		return cat.orElseThrow(()-> new ObjectNotFoundException("Categoria não encontrada"));
 	}
 }
