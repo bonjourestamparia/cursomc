2ebaf47300122258ff3eac66954c6b7c9d7c228e
Rodrigo Bonjour <rodrigo@bonjourestamparia.com.br>
2020-03-29 22:01:05 -0300

    Pedido e item pedido

--- src/main/java/com/bonjourestamparia/CursomcApplication.java
+++ src/main/java/com/bonjourestamparia/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.bonjourestamparia.domain.Cidade;
 import com.bonjourestamparia.domain.Cliente;
 import com.bonjourestamparia.domain.Endereco;
 import com.bonjourestamparia.domain.Estado;
+import com.bonjourestamparia.domain.ItemPedido;
 import com.bonjourestamparia.domain.Pagamento;
 import com.bonjourestamparia.domain.PagamentoComBoleto;
 import com.bonjourestamparia.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.bonjourestamparia.repositories.CidadeRepository;
 import com.bonjourestamparia.repositories.ClienteRepository;
 import com.bonjourestamparia.repositories.EnderecoRepository;
 import com.bonjourestamparia.repositories.EstadoRepository;
+import com.bonjourestamparia.repositories.ItemPedidoRepository;
 import com.bonjourestamparia.repositories.PagamentoRepository;
 import com.bonjourestamparia.repositories.PedidoRepository;
 import com.bonjourestamparia.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -121,6 +126,20 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		// Itens de pedido
+		ItemPedido ip1 = new ItemPedido(ped1,  p1, 0.00, 1, 200.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
--- src/main/java/com/bonjourestamparia/domain/Produto.java
+++ src/main/java/com/bonjourestamparia/domain/Produto.java
@@ -2,7 +2,10 @@ package com.bonjourestamparia.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +15,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -33,6 +37,9 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<ItemPedido>();
+	
 	public Produto() {
 	}
 	
@@ -82,6 +89,18 @@ public class Produto implements Serializable{
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
+	public List<Pedido> getPedidos(){
+		return (itens.stream().map(ItemPedido::getPedido).collect(Collectors.toList()));		
 	}
 
 	@Override
