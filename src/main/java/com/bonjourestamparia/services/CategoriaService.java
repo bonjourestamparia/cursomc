7df45e744752d6c75c19dca316cd9ef10b39775f
Rodrigo Bonjour <rodrigo@bonjourestamparia.com.br>
2020-03-30 22:50:34 -0300

    Listando todas categorias com DTO

--- src/main/java/com/bonjourestamparia/services/CategoriaService.java
+++ src/main/java/com/bonjourestamparia/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.bonjourestamparia.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.bonjourestamparia.domain.Categoria;
 import com.bonjourestamparia.repositories.CategoriaRepository;
+import com.bonjourestamparia.services.exceptions.DataIntegrityException;
 import com.bonjourestamparia.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +25,23 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria obj) {
 		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		buscar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}	
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
