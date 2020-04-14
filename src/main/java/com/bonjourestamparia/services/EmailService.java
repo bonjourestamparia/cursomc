1bfb32a916d65fb4523d57d9bf3e06705f2725ef
Rodrigo Bonjour <rodrigo@bonjourestamparia.com.br>
2020-04-14 00:42:15 -0300

    Envio de email html

--- src/main/java/com/bonjourestamparia/services/EmailService.java
+++ src/main/java/com/bonjourestamparia/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.bonjourestamparia.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.bonjourestamparia.domain.Pedido;
@@ -7,5 +9,9 @@ import com.bonjourestamparia.domain.Pedido;
 public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/bonjourestamparia/services/MockMailService.java
+++ src/main/java/com/bonjourestamparia/services/MockMailService.java
@@ -1,5 +1,7 @@
 package com.bonjourestamparia.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -9,6 +11,13 @@ public class MockMailService extends AbstractEmailService{
 	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
+		LOG.info("Simulando envio de email");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado com sucesso!");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
 		LOG.info("Simulando envio de email");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado com sucesso!");
