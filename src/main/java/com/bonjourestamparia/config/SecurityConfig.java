b56e21aee48b3b8d17ce064306e7a950953f11a5
Rodrigo Bonjour <rodrigo@bonjourestamparia.com.br>
2020-04-23 21:28:56 -0300

    Implementando autorização

--- src/main/java/com/bonjourestamparia/config/SecurityConfig.java
+++ src/main/java/com/bonjourestamparia/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.bonjourestamparia.security.JWTAuthenticationFilter;
+import com.bonjourestamparia.security.JWTAuthorizationFilter;
 import com.bonjourestamparia.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
